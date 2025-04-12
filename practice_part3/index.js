eaee2be5ea66c75b27d26ae9138dbfa430748955
soomelauri <las623@lehigh.edu>
2025-04-12 01:49:59 -0400

    Part 3 (a)

--- practice_part3/index.js
+++ practice_part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 const http = require('http')
 
@@ -27,6 +28,54 @@ let notes = [
   
   app.get('/api/notes', (request, response) => {
     response.json(notes)
+  })
+
+  // fetch a single resourc
+  app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+      res.json(note)
+    } else {
+      res.status(404).end()
+    }
+  })
+
+  // delete a rensource
+  app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    notes = notes.filter(note => note.id !== id)
+    res.status(204).end()
+  })
+
+  // Generate an id for the note
+  const generateId = () => {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+
+    return String(maxId + 1)
+  }
+
+  // create a resource using POST
+  app.post('/api/notes', (req, res) => {
+    
+    if (!body.content) {
+      return res.status(400).json({
+        error: "Content missing"
+      })
+    }
+
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      id: generateId()
+    }
+
+    notes = notes.concat(note)
+
+    res.json(note)
   })
   
 const PORT = 3001
