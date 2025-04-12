295d3e4040c8c0db391ecbb3dc20b60b4144561f
soomelauri <las623@lehigh.edu>
2025-04-12 15:39:42 -0400

    3.3: Phonebook backend step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -24,10 +24,29 @@ const persons = [
     }
 ]
 
+const phonebook_length = persons.length
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+// get specific person from the phonebook
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(person => person.id === id)
+  res.json(person)
+})
+
+// info page
+// calculate the length of phonebook - then output that in the info
+app.get('/info', (req, res) => {
+  res.send(`Phonebook has info for ${phonebook_length} people`)
+
+})
+
+
+
 const PORT = 3000
 app.listen(PORT)
 console.log(`To see the app, go to http://localhost:${PORT}`)
+console.log(phonebook_length)
