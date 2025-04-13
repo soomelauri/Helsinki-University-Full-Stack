c48d2f3b5459f9b90f3265882dceb438336257a4
soomelauri <las623@lehigh.edu>
2025-04-13 14:52:05 -0400

    Updated Part3 practice

--- practice_part3/index.js
+++ practice_part3/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-app.use(express.json())
 
 const http = require('http')
 
@@ -20,63 +19,83 @@ let notes = [
       content: "GET and POST are the most important methods of HTTP protocol",
       important: true
     }
-  ]
+]
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
+  next()
+}
+// use () to return middleware
+app.use(express.json())
+// no need for () due to the function already being directly usable
+app.use(requestLogger)
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+// fetch a single resourc
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+// delete a rensource
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
+
+// Generate an id for the note
+const generateId = () => {
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => Number(n.id)))
+  : 0
+
+  return String(maxId + 1)
+}
+
+// create a resource using POST
+app.post('/api/notes', (request, response) => {
   
-  app.get('/api/notes', (request, response) => {
-    response.json(notes)
-  })
-
-  // fetch a single resourc
-  app.get('/api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-      res.json(note)
-    } else {
-      res.status(404).end()
-    }
-  })
-
-  // delete a rensource
-  app.delete('/api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
-  })
-
-  // Generate an id for the note
-  const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-
-    return String(maxId + 1)
+  if (!body.content) {
+    return response.status(400).json({
+      error: "Content missing"
+    })
   }
 
-  // create a resource using POST
-  app.post('/api/notes', (req, res) => {
-    
-    if (!body.content) {
-      return res.status(400).json({
-        error: "Content missing"
-      })
-    }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }
 
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      id: generateId()
-    }
+  notes = notes.concat(note)
 
-    notes = notes.concat(note)
+  response.json(note)
+})
 
-    res.json(note)
-  })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'endpoint not found'})
+}
+
+// no need for () due to the function already being directly usable
+app.use(unknownEndpoint)
   
 const PORT = 3001
 app.listen(PORT)
