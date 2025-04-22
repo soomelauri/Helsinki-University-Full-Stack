2771c60d828364f65db5821308acef1bc5a6aeb5
soomelauri <las623@lehigh.edu>
2025-04-21 20:24:09 -0400

    4.2 Blog List, step 2
    Completed refactoring

---
+++ part4/blog_list/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config.js')
+const logger = require('./utils/logger.js')
+
+const blogRouter = require('./controllers/blogs.js')
+
+// create express app
+const app = express()
+
+// state that we are connecting to DB
+logger.info('Connecting to DB', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Successfully connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error', error.message)
+  })
+
+// Use Middleware
+app.use(express.json())
+
+// use router
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,22 +1,7 @@
-const express = require('express')
-const mongoose = require('mongoose')
 const config = require('./utils/config.js')
-
 const logger = require('./utils/logger.js')
 
-const blogRouter = require('./controllers/blogs.js')
-
-// create express app
-const app = express()
-
-// store the mongoDBURL
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-// Use Middleware
-app.use(express.json())
-
-app.use('/api/blogs', blogRouter)
+const app = require('./app.js')
 
 // Define PORT and set the app to listen to that port
 app.listen(config.PORT, () => {
