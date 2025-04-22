9e60858e52f4a78b921b028d37db9a872038e0a2
soomelauri <las623@lehigh.edu>
2025-04-21 23:39:55 -0400

    4.3: Helper Functions and Unit Tests, step 1

---
+++ part4/blog_list/utils/list_helper.js
@@ -0,0 +1,11 @@
+// Let's create a collection of helper functions that are best suited for working with the describe sections of the blog list. 
+// Create the functions into a file called utils/list_helper.js.
+//  Write your tests into an appropriately named test file under the tests directory.
+
+const dummy = (blogs) => {
+  return 1
+}
+
+module.exports = {
+    dummy
+}
