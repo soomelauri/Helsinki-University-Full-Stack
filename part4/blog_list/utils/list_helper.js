71eee54cd700eb8fc10c5fb258697b0f1274b323
soomelauri <las623@lehigh.edu>
2025-04-22 00:31:14 -0400

    4.4: Helper Functions and Unit Tests, step 2

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -6,6 +6,24 @@ const dummy = (blogs) => {
   return 1
 }
 
+// Define a new totalLikes function that receives a list of blog posts as a parameter. 
+// The function returns the total sum of likes in all of the blog posts.
+// Write appropriate tests for the function. 
+// It's recommended to put the tests inside of a describe block so that the test report output gets grouped 
+// nicely:
+
+// I think this is a reducer function, you take a list, keep count, and then iterate
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.length === 0 
+      ? 0
+      : blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
