db06fde20908d2bb0b2a89b2ab1a542056cf28ba
soomelauri <las623@lehigh.edu>
2025-04-23 10:39:43 -0400

    Practice update Part 4

--- practice_part4/tests/note_api.test.js
+++ practice_part4/tests/note_api.test.js
@@ -9,13 +9,10 @@ const helper = require('./test_helper.js')
 const Note = require('../models/note')
 
 beforeEach(async () => {
+  // for this beforeEach function, let's use a for-loop to iterate over the array
+  // and then
   await Note.deleteMany({})
-
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  await Note.insertMany(helper.initialNotes)
 })
 
 test('notes are returned as json', async () => {
