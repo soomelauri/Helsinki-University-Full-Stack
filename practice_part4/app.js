d24bdd1d77bcc0f633b9e3a340dad504da5ae832
soomelauri <las623@lehigh.edu>
2025-04-25 17:37:16 -0400

    Part 4 - Token auth

--- practice_part4/app.js
+++ practice_part4/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config.js')
 const logger = require('./utils/logger')
 const notesRouter = require('./controllers/notes.js')
 const usersRouter = require('./controllers/users.js')
+const loginRouter = require('./controllers/login.js')
 const middleware = require('./utils/middleware.js')
 
 // initialize the app
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- practice_part4/controllers/notes.js
+++ practice_part4/controllers/notes.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', asyncHandler(async (request, response) => {
   const notes = await Note.find({}).populate('user', { name: 1, username: 1 })
@@ -19,10 +20,26 @@ notesRouter.get('/:id', asyncHandler(async (request, response) => {
   }
 }))
 
+// Make sure the token is sent with the notes post request
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.post('/', asyncHandler(async (request, response) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
