52dc41d1c9ac42a107c1f1b3f6809a59120860b3
soomelauri <las623@lehigh.edu>
2025-04-28 12:22:27 -0400

    5.1: Blog List Frontend, step 1

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    const user = await loginService.login({ username, password })
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  }
+
+  const loginForm = () => {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input type="text"
+          value={username}
+          name="Username"
+          onChange={({target}) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input type="password"
+          value={password}
+          name="Password"
+          onChange={({target}) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    )
+  }
+
+  return (
+    <div>
+      {user === null 
+        ? (
+          <div>
+            <h2>Log in to application</h2>
+            {loginForm()}
+          </div>
+        )
+        :
+        <div>
+          <h2>blogs</h2>
+          <p>{user.name} logged-in</p>
+          {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+        </div>
+      }
+    </div>
+  )
+}
+
+export default App
