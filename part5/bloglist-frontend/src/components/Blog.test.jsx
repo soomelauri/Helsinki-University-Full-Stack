7b14c83285e924bc405410814e61eac918556686
soomelauri <las623@lehigh.edu>
2025-05-01 12:42:44 -0400

    5.15: Blog List Tests, step 3
    Test for the like button

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
-import { expect } from 'vitest'
+import { expect, test } from 'vitest'
 import userEvent from '@testing-library/user-event'
 
 // let's make sure that we render the first component and the author and the title, and not the url and the likes
@@ -18,8 +18,6 @@ test('blog title and author rendered', () => {
     const user = {username: 'ls1996'}
 
     const { container } = render(<Blog blog={blog} user={user}/>)
-
-    screen.debug()
 
     const div = container.querySelector('.blog')
 
@@ -58,9 +56,6 @@ test('when button clicked, url and likes are shown', async () => {
     // click
     await blogUser.click(button)
 
-    // let's check the output
-    screen.debug()
-
     // check if likes and url found in their respective components
     const likesDiv = container.querySelector('.blogLikes')
     const urlDiv = container.querySelector('.blogUrl')
@@ -68,3 +63,49 @@ test('when button clicked, url and likes are shown', async () => {
     expect(likesDiv).toHaveTextContent('100', { exact: false })
     expect(urlDiv).toHaveTextContent('http://localhost:5173', { exact: false })
 })
+
+// Make a test, which ensures that if the like button is clicked twice, the event handler 
+// the component received as props is called twice.
+
+
+  test('like button called twice is registered', async () => {
+    // data sent through
+    const blog = {
+        title: 'Title',
+        author: 'Blog Author',
+        url: 'http://localhost:5173',
+        likes: 100,
+        user: {username: 'ls1996'}
+      }
+
+    // user matching the check
+    const user = {username: 'ls1996'}
+
+    // function handler
+    const updateBlogHandler = vi.fn()
+
+    // component rendering
+    render(<Blog blog={blog} user={user} updateBlog={updateBlogHandler} />)
+
+
+    // set up user event
+    const testUser = userEvent.setup()
+    // set up the view button
+    const viewButton = screen.getByText('view')
+
+    // call the user event, clicking the view button
+    await testUser.click(viewButton)
+
+    // save the like button
+    const likeButton = screen.getByText('like')
+
+    // click the like button twice
+    await testUser.click(likeButton)
+    await testUser.click(likeButton)
+
+    // check that the likeButton was clicked twice
+    expect(updateBlogHandler.mock.calls).toHaveLength(2)
+  })
+
+
+
